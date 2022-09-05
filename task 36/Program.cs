88fea4fe7bbe4c0289335fb68b174783bcf59f8d
Anna Galieva <annet_forever08@mail.ru>
2022-09-05 11:44:38 +0300

    task 36

---
+++ task 36/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях(по индексу считаем).
+
+int[] arr = new int[5];
+void RandomArray(int[]array)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Random rnd = new Random();
+        int value = rnd.Next(-100,100);
+        arr[i] = value;
+        Console.WriteLine($"{arr[i]}");
+    }
+}
+RandomArray(arr);
+
+void GetOddSum(int[] array)
+{
+    int oddSum = 0;
+    for (int i = 0; i < arr.Length; i+=2)
+    {
+        oddSum += arr[i];
+    }
+    Console.WriteLine($"Сумма элементов с нечетным индексом = {oddSum}");
+}
+GetOddSum(arr);
